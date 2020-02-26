c86a51a301895b579e02fc8e962c91c54b053e5c
Rayna Levinson <rlevinso@localhost.localdomain>
2020-02-26 14:46:14 -0500

    sentiment_analysis mlperf tekton pipeline

---
+++ sentiment_analysis/util/get_logs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+POD=`echo $(oc get pods -o name) | grep -o 'pod/tf-build-pipeline-run-run[^ ]*'`
+
+oc logs -f $POD --container='step-run' > logs.txt
+sed -i '/\[=/d' logs.txt
+IFS=',' read -ra RESULT <<< `tail -n 1 'logs.txt'`
+
+tee result.txt << EOF
+${RESULT[0]}
+
+NAME: ${RESULT[1]}
+SEED: ${RESULT[2]}
+TIME: ${RESULT[3]} sec
+USER: ${RESULT[4]}
+START: ${RESULT[5]}
+EOF
