647d42247265bc89a57900498fcc5bc90741494e
Rayna Levinson <rlevinso@localhost.localdomain>
2020-03-20 15:40:14 -0700

    object detection cleanup
    moving pvcs to storage folder
    
    adding seperate download task

--- object_detection/setup.sh
+++ object_detection/setup.sh
@@ -3,6 +3,7 @@ oc create -f misc/imagestream.yml
 oc create -f pipeline/resources.yml
 oc create -f task/buildah.yaml
 oc create -f task/run-benchmark.yml
+oc create -f task/dataset.yml
 oc create -f pipeline/pipeline.yml
-oc create -f pipeline/dataset-pvc.yml
-oc create -f pipeline/runtime-pvc.yml
+oc create -f storage/dataset-pvc.yml
+oc create -f storage/runtime-pvc.yml
