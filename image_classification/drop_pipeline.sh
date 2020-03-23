c446348108b8218b367c0f5e3cc2c615b132be5b
Rayna Levinson <rlevinso@localhost.localdomain>
2020-03-23 13:47:12 -0700

    first pass at generalizing pipeline

--- image_classification/drop_pipeline.sh
+++ image_classification/drop_pipeline.sh
@@ -1,6 +1,3 @@
-oc delete pipeline/image-classification
 oc delete pipelinerun/image-classification-pipeline-run
 oc delete pipelineresource/ic-build-image
 oc delete pipelineresource/ic-repo
-oc delete task/ic-buildah
-oc delete task/ic-run
